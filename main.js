d01c7b92df4b385a88527109467a5db67bf06497
beurmuz <fallinta2@gmail.com>
2021-08-19 18:57:46 +0900

    홈화면 버튼 추가, flex 적용

---
+++ main.js
@@ -0,0 +1,19 @@
+const Btn1 = document.querySelector('#Btn1');
+const Btn2 = document.querySelector('#Btn2');
+const Btn3 = document.querySelector('#Btn3');
+
+Btn1.addEventListener('click', () => {
+    window.open('./src/NumberBaseball/NumberBaseball.html');
+});
+
+Btn2.addEventListener('click', () => {
+    window.open('./src/CatchOgu/CatchOgu.html');
+});
+
+Btn3.addEventListener('click', () => {
+    window.open('./src/2048/2048.html');
+});
+
+// (() => {
+
+// });
